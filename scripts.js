fe66805b2ef3c2eebb221caa733a47397b451200
basuabhirup <basu.abhirup@yahoo.com>
2021-09-04 01:36:15 +0530

    Set randomTIme and randomHole functions.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
+
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes){
+  const hole = holes[Math.floor(Math.random() * holes.length)];
+  if(hole === lastHole) {
+    console.log("Ahhhhhaaaaaa the buds are repeating repeaing more than one repeat !");
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
